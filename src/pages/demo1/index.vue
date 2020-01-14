e3d25d475a3459537630e84353436855ce769fb2
LuciusTan <tanliang1314@hotmail.com>
2020-01-15 00:56:14 +0800

    git+vue LEARNING

---
+++ src/pages/demo1/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="test">Hello Vue</div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    beforeCreate(){
+        console.log("beforeCreate")
+    }, 
+    created(){
+        console.log("beforeCreate")
+    }, 
+    beforeMount(){
+        console.log("beforeMount")
+    }, 
+    mounted(){
+        console.log("mounted")
+    }, 
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Hello from '@/pages/demo1/index.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/demo1',
+      name: 'demo1',
+      component: Hello
+    }   
+
+  ]
+})
