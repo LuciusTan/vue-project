b8c493c85f17412fb7d39ba44da7ca685a580586
LuciusTan <tanliang1314@hotmail.com>
2020-01-15 10:10:36 +0800

    create demo2

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Hello from '@/pages/demo1/index.vue'
+import demo2 from '@/pages/demo2/index.vue'
 
 Vue.use(Router)
 
@@ -16,7 +17,12 @@ export default new Router({
       path: '/demo1',
       name: 'demo1',
       component: Hello
-    }   
+    },
+    {
+      path: '/demo2',
+      name: 'demo2',
+      component: demo2
+    }
 
   ]
 })
